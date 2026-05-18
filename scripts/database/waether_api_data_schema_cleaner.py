9e910612860999877b4bdb502569533bc149e1e0
Pablo <pawelkozikowski58@gmail.com>
2026-05-18 10:49:05 +0200

    Clenaer for our database

--- scripts/database/waether_api_data_schema_cleaner.py
+++ scripts/database/waether_api_data_schema_cleaner.py
@@ -4,16 +4,16 @@ from __future__ import annotations
 from copy import deepcopy
 from typing import final, Any
 from logging import Logger
-from schema_metadata_store import ColumnMetadata, SchemaMetadataStore, TableSchemaMetadata
+from schema_metadata_store import SchemaMetadataStore, TableSchemaMetadata
 from step_raport_generator import StepRaportGenerator
-from data_schema_cleaner import CleaningStep
+from data_schema_cleaner import CleaningResults, CleaningStep
 from database_payload import DatabasePayload
-from database_types import DatabaseRow
+from database_types import DatabaseRow, FailedRow
 
 
 class ToPostgreSQLDataSchemaCleaner:
     """Cleaner for data incoming from source to PostgreSQL database."""
-    
+
     def __init__(
         self,
         schema_store: SchemaMetadataStore,
@@ -30,7 +30,7 @@ class ToPostgreSQLDataSchemaCleaner:
         self.cleaning_steps.clear()
 
     def _clean_row(self, row: DatabaseRow, table_metadata: TableSchemaMetadata) -> DatabaseRow:
-        # TODO - add documentation for this method 
+        # TODO - add documentation for this method
         # TODO - think more about possible cleaning solution for rows with invalid data
         cleared_row: DatabaseRow = deepcopy(row)
         for column_name, column_value in row.items():
@@ -38,19 +38,18 @@ class ToPostgreSQLDataSchemaCleaner:
                 # TODO - log warning that column is not in table metadata
                 _ = cleared_row.pop(column_name)
                 continue
-            
+
             if column_name in table_metadata.required_column_names and column_value is None:
                 # TODO - log warning that required column is not provided and column in database is not nullable
                 _ = cleared_row.pop(column_name)
                 continue
-        
+
         return cleared_row
-        
 
     def clean_data(
         self, data: DatabasePayload, *args: tuple, **kwargs: dict[str, Any]
-    ) -> DatabasePayload:
-        # TODO - add documentation for this method 
+    ) -> CleaningResults:
+        # TODO - add documentation for this method
         table_name: str | None = kwargs.get("table_name", None)
         if table_name is None:
             raise ValueError("table_name keyword is not specified!")
@@ -60,6 +59,7 @@ class ToPostgreSQLDataSchemaCleaner:
             raise ValueError(f"Cannot provided table in schema with provided name: {table_name}")
         self.clean_buffer()
         cleaned_rows: list[DatabaseRow] = []
+        failed_rows: list[FailedRow] = []
 
         for row in data.rows:
             try:
@@ -74,6 +74,14 @@ class ToPostgreSQLDataSchemaCleaner:
                     )
                 )
             except Exception as exep:
+                failed_rows.append(
+                    FailedRow(
+                        table_name=table_name,
+                        row=row,
+                        message=f"Invalid row cleaning - {str(exep)}",
+                        exception=exep,
+                    )
+                )
                 self.cleaning_steps.append(
                     CleaningStep(
                         data_before=row,
@@ -83,6 +91,12 @@ class ToPostgreSQLDataSchemaCleaner:
                     )
                 )
 
-        return DatabasePayload(
-            rows=cleaned_rows, number_of_rows=len(cleaned_rows), request_type=data.request_type
+        return CleaningResults(
+            cleaned_payload=DatabasePayload(
+                rows=cleaned_rows,
+                number_of_rows=len(cleaned_rows),
+                created_at=data.created_at,
+                request_type=data.request_type,
+            ),
+            failed_rows=failed_rows,
         )
