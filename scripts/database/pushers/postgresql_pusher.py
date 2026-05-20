9a6ed59c2e03ec9c1cb53fb719b750f54d6e7c76
Pablo <pawelkozikowski58@gmail.com>
2026-05-20 21:18:41 +0200

    Added normalization for queries and params

--- scripts/database/pushers/postgresql_pusher.py
+++ scripts/database/pushers/postgresql_pusher.py
@@ -2,6 +2,7 @@ from typing import Any
 
 import psycopg
 from psycopg import Connection
+from psycopg import sql
 from psycopg.rows import dict_row
 
 from scripts.database.models.database_types import DatabaseRow, DatabaseOperationResult, FailedRow
@@ -23,18 +24,30 @@ class PostgreSQLDatabasePusher:
         query_builder = self.builder_t(table_name=table_name, data=data)
         return query_builder.get_query(), query_builder.get_params()
 
+    def _normalize_query_param(self, q: Any, p: Any) -> dict:
+        # Maps queries to params
+        # Batch version
+        if isinstance(q, dict) and isinstance(p, dict):
+            assert q.keys() == p.keys()
+            return {q[columns_key]: p[columns_key] for columns_key in list(q.keys())}
+
+        # Single row version
+        elif isinstance(q, sql.Composed) and isinstance(p, tuple):
+            return {q: p}
+        
+        else:
+            raise TypeError("Unsupported type of quries or params")
+
     def insert(self, table_name: str, rows: list[DatabaseRow]) -> DatabaseOperationResult:
         if not rows:
             return DatabaseOperationResult(successful_rows=[], failed_rows=[])
         query, params = self._prepare_query_and_params(table_name, rows)
+        query_params_map: dict = self._normalize_query_param(query, params)
 
         with self.connection.cursor() as conn_cursor:
 
             try:
-                if isinstance(query, dict) and isinstance(params, dict):
-                    for columns, insert_query in query.items():
-                        conn_cursor.execute(insert_query, params[columns])
-                else:
+                for query, params in query_params_map.items():
                     conn_cursor.execute(query, params)
 
                 if not self.autocommit:
